0bf3535b7957e79217e4947f4cde4a7f23239ea4
Tufail Ahmed <tufailahmedkhan@gmail.com>
2021-09-08 12:47:37 +0530

    tufail third commit

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pavan, How r u today" );
+        System.out.println( "Hello Tufail Ahmed, How r u today" );
     }
 }
